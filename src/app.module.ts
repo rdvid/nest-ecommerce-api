8cbedc98a1771ff1cb7cf1e7f3d3fd792469dbef
Rafael <rafael_dvid@hotmail.com>
2023-10-30 20:14:28 -0300

    setting typeorm/remote pgdb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule, 
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [ PostgresConfigService ]
+    }),
+
+  ],
 })
 export class AppModule {}
